2066e75743f12bfd966281013587ba9c982de572
Marcin Konarski <marcin.konarski@codestation.org>
2015-06-22 03:13:37 +0200

    Fix indentation in preamble.

--- src/main.cxx
+++ src/main.cxx
@@ -1,7 +1,7 @@
 /*
 ---            `Skeleton' 0.0.0 (c) 1978 by Marcin 'Amok' Konarski             ---
 
-	main.cxx - this file is integral part of `Skeleton' project.
+  main.cxx - this file is integral part of `Skeleton' project.
 
   i.  You may not make any changes in Copyright information.
   ii. You must attach Copyright information to any part of every copy
